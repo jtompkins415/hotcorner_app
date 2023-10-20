d770a5f8be303f0a0f9e818dd22ba595984b9be6
jtompkins415 <john.tompkins415@gmail.com>
2023-10-20 08:43:20 -0700

    write methods and constructors for Order class

--- Models/Order.cs
+++ Models/Order.cs
@@ -26,6 +26,69 @@ public class Order
         //Properties
         public Guid MenuItemId {get; set;}
         public string MenuItemName {get; set;}
+        public decimal MenuItemPrice {get; set;}
         public int Quantity {get; set;}
     }
+
+    public Order()
+    {
+        OrderItems = new List<OrderItem>();
+    }
+
+    public Order(int orderNum, Employee employee, DateTime orderTime, Table table, List<OrderItem> orderItems, string status, decimal totalPrice)
+    {
+        OrderId = Guid.NewGuid();
+        OrderNum = orderNum;
+        EmployeeId = employee.EmployeeId;
+        EmployeeName = employee.EmployeeName;
+        OrderTime = orderTime;
+        TableId = table.TableId;
+        TableNumber = table.TableNumber;
+        OrderItems = orderItems;
+        Status = status;
+        TotalPrice = totalPrice;
+    }
+
+    //Methods
+
+    //AddItem: Allows adding a new item to the order
+    public void AddItem(MenuItem menuItem, int quantity)
+    {
+        //Add specified item to the order's list of items
+        OrderItems.Add(new OrderItem
+        {
+            MenuItemId = menuItem.Id,
+            MenuItemName = menuItem.Name,
+            MenuItemPrice = menuItem.Price,
+            Quantity = quantity
+
+        });
+    }
+
+    //RemoveItem: Allows removal of an item from the order based on the menu item ID
+    public void RemoveItem(MenuItem menuItem)
+    {
+        //Find and remove the item with the specified ID
+        var itemToRemove = OrderItems.FirstOrDefault(item => item.MenuItemId == menuItem.Id);
+        if(itemToRemove != null)
+        {
+            OrderItems.Remove(itemToRemove);
+        }
+    }
+
+    /*CalculateTotalPrice: Calculates the total price of the order based on the individual items and their quanities;
+        Updates the `TotalPrice` property
+    */
+    public void CalculateTotalPrice()
+    {
+        TotalPrice = OrderItems.Sum(item => item.Quantity * item.MenuItemPrice);
+    }
+
+    //ChangeStatus: Changes the status of the order
+    public void ChangeStatus(string newStatus)
+    {
+        Status = newStatus;
+    }
+
+    
 }
