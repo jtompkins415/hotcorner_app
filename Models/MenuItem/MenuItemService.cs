8a4ce77b06e2533440f02041867cf62c6780793c
jtompkins415 <john.tompkins415@gmail.com>
2023-10-23 20:54:34 -0700

    Create MenuItem Directory and CRUD service class

---
+++ Models/MenuItem/MenuItemService.cs
@@ -0,0 +1,33 @@
+// Public class for CRUD operations on the MenuItems table
+
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using ConsoleApp.PostgreSQL;
+using HotCorner.Model;
+
+namespace HotCorner.Model
+{
+    public class MenuItemService
+    {
+        private readonly AppDbContext _context;
+
+        public MenuItemService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //AddMenuItemAsync: Add new MenuItem to MenuItems Table
+        public async Task AddMenuItemAsync(MenuItem menuItem)
+        {
+            _context.MenuItems.Add(menuItem);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveMenuItemAsync(int menuItemId)
+        {
+            MenuItem menuItem = await _context.MenuItems.FindAsync(menuItemId);
+            _context.Remove(menuItem);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
