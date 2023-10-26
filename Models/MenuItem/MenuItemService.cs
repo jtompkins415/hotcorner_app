ac8ea427a9979758f19c7cde5c374295cb48bb1b
jtompkins415 <john.tompkins415@gmail.com>
2023-10-25 22:30:36 -0700

    Add Read, Update methods and update error handling

--- Models/MenuItem/MenuItemService.cs
+++ Models/MenuItem/MenuItemService.cs
@@ -1,7 +1,6 @@
 // Public class for CRUD operations on the MenuItems table
-
+                             
 using Microsoft.EntityFrameworkCore;
-using System.Threading.Tasks;
 using ConsoleApp.PostgreSQL;
 
 namespace HotCorner.Model
@@ -13,13 +12,29 @@ namespace HotCorner.Model
         public MenuItemService(AppDbContext context)
         {
             _context = context;
-        }
+        }                                                 
 
         //AddMenuItemAsync: Add new MenuItem to MenuItems Table
         public async Task AddMenuItemAsync(MenuItem menuItem)
         {
-            _context.MenuItems.Add(menuItem);
-            await _context.SaveChangesAsync();
+            try
+            {
+                _context.MenuItems.Add(menuItem);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+            
+        }
+
+
+        //ReadMenuItemAsync: Returns MenuItem based on a given ID
+        public async Task<MenuItem> ReadMenuItemAsync(int menuItemId)
+        {
+            MenuItem menuItem = await _context.MenuItems.FindAsync(menuItemId);
+            return menuItem;
         }
 
         //RemoveMenuItemAsyc: Remove MenuItem by ID
@@ -28,6 +43,46 @@ namespace HotCorner.Model
             MenuItem menuItem = await _context.MenuItems.FindAsync(menuItemId);
             _context.Remove(menuItem);
             await _context.SaveChangesAsync();
+        }
+
+        //UpdateMenuItemAsync: Update properties of an existing MenuItem
+        public async Task<bool> UpdateMenuItemAsync(
+            int menuItemId,
+            string name = null,
+            string description = null,
+            decimal? price = null,
+            string category = null
+        )
+        {
+            MenuItem itemToUpdate = await _context.MenuItems.FirstOrDefaultAsync(m => m.Id == menuItemId);
+
+            if(itemToUpdate == null)
+            {
+                return false;
+            }
+
+            if(name != null)
+            {
+                itemToUpdate.Name = name;
+            }
+            if(description != null)
+            {
+                itemToUpdate.Description = description;
+            }
+            if(price != null)
+            {
+                itemToUpdate.Price = price.Value;
+            }
+            if(category != null)
+            {
+                itemToUpdate.Category = category;
+            }
+
+            _context.Update(itemToUpdate);
+
+            await _context.SaveChangesAsync();
+
+            return true;
         }
     }
 }
